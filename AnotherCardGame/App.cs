3016d916e2d529acd3bddfb94d1251f36846704d
PavelAltynnikov <robenzon11@gmail.com>
2023-08-01 19:04:36 +0400

    add application class

---
+++ AnotherCardGame/App.cs
@@ -0,0 +1,50 @@
+﻿// TODO
+// [x] Написать Card
+// [x] Написать CardFactory
+// Написать Enemy
+// Написать EnemyFactory
+// Написать Round
+// Написать Game
+// Написать Controller
+
+using AnotherCardGame.GameObjects;
+
+var cardsNames = GetCardsNames();
+var cardFactory = new CardFactory(cardsNames);
+
+var card = cardFactory.GetRandomCard();
+Console.WriteLine(card.Name);
+Console.WriteLine(card.Strength);
+Console.WriteLine(card.Elemental);
+Console.WriteLine(card.Alignment.Value);
+
+List<string> GetCardsNames()
+{
+    // Давайте представим, что эти имена приходят из файла или DB или ещё откуда.
+    // В данном случае из jojo.fandom.com
+    return new List<string>
+    {
+        "Дурак",
+        "Маг",
+        "Верховная Жрица",
+        "Императрица",
+        "Император",
+        "Верховный Жрец",
+        "Любовники",
+        "Колесница",
+        "Сила",
+        "Отшельник",
+        "Колесо Фортуны",
+        "Справедливость",
+        "Повешенный",
+        "Смерть",
+        "Умеренность",
+        "Дьявол",
+        "Башня",
+        "Звезда",
+        "Луна",
+        "Солнце",
+        "Суд",
+        "Мир",
+    };
+}
