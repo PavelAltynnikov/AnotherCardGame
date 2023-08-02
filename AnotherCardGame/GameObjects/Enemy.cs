8cbe39b7b2dabbca5406a249d78a1128ef97be80
PavelAltynnikov <robenzon11@gmail.com>
2023-08-02 12:10:55 +0400

    add enemy classes

---
+++ AnotherCardGame/GameObjects/Enemy.cs
@@ -0,0 +1,13 @@
+﻿using AnotherCardGame.GameObjects.Characteristics;
+
+namespace AnotherCardGame.GameObjects;
+
+public class Enemy : BattleObject
+{
+    public Enemy(string name, int health, Elemental elemental, Alignment alignment) : base(name, elemental, alignment)
+    {
+        Health = health;
+    }
+
+    public int Health { get; }
+}
---
+++ AnotherCardGame/GameObjects/EnemyFactory.cs
@@ -0,0 +1,28 @@
+﻿using AnotherCardGame.GameObjects.Characteristics;
+
+namespace AnotherCardGame.GameObjects;
+
+public class EnemyFactory : AbstractBattleObjectFactory
+{
+    private readonly Random _random = new();
+
+    public EnemyFactory(List<string> names) : base(names) { }
+
+    public override Enemy GetRandomItem()
+    {
+        return new Enemy(
+            ChooseRandomName(),
+            ChooseRandomHealth(),
+            Enum<Elemental>.PickRandom(),
+            new Alignment(
+                Enum<Moral>.PickRandom(),
+                Enum<Ethics>.PickRandom()
+            )
+        );
+    }
+
+    private int ChooseRandomHealth()
+    {
+        return _random.Next(1, 20);
+    }
+}
